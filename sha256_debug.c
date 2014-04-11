81880fb0848592a578fa66279e40d81d52bf4dd6
Iburinoc <iburinoc@gmail.com>
2014-04-11 18:22:52 -0400

    added sha256 test vectors, not proper test yet

--- sha256_debug.c
+++ sha256_debug.c
@@ -3,15 +3,39 @@
 #include "util.c"
 
 int main() {
-	unsigned char* word = (unsigned char*)"abc";
+	{
+		unsigned char* word = (unsigned char*)"abc";
 	
-	unsigned char out[64];
+		unsigned char hash[32];
+		hash_sha256(word, 3, hash);
+		printbuf(hash, 32);
+	}
+	{
+		unsigned char* word = (unsigned char*)"";
 	
+		unsigned char hash[32];
+		hash_sha256(word, 0, hash);
+		printbuf(hash, 32);
+	}
+	{
+		unsigned char* word = (unsigned char*)"The quick brown fox jumps over the lazy dog";
 	
-	unsigned char hash[32];
-	hash_sha256(word, 3, hash);
-	printbuf(hash, 32);
+		unsigned char hash[32];
+		hash_sha256(word, 43, hash);
+		printbuf(hash, 32);
+	}
+	{
+		unsigned char* word = (unsigned char*)"The quick brown fox jumps over the lazy dog.";
 	
-	unsigned int i = 0x80000000;
-	printf("%x\n", i >> 1);
+		unsigned char hash[32];
+		hash_sha256(word, 44, hash);
+		printbuf(hash, 32);
+	}
+	{
+		unsigned char* word = (unsigned char*)"abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz";
+	
+		unsigned char hash[32];
+		hash_sha256(word, 234, hash);
+		printbuf(hash, 32);
+	}
 }

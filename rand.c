e5ddac7e43ed89b4824454d2f3a9655ec8efa8be
Iburinoc <iburinoc@gmail.com>
2014-04-17 11:13:44 -0400

    Fixed rand.c compiler issues

--- rand.c
+++ rand.c
@@ -1,6 +1,10 @@
+#include <errno.h>
+#include <unistd.h>
 #include <fcntl.h>
 #include <stdlib.h>
 #include <stdio.h>
+
+#include "rand.h"
 
 // returns URANDOM_FAIL if unsuccessful, 0 if successful
 int cs_rand(uint8_t* buf, uint32_t buflen) {
@@ -12,7 +16,7 @@ int cs_rand(uint8_t* buf, uint32_t buflen) {
 	}
 	
 	while(buflen > 0) {
-		if((lenread = read(fd, buf, buflen, 0) == 0) {
+		if((lenread = read(fd, buf, buflen)) == 0) {
 			// /dev/urandom should never EOF
 			goto err1;
 		}

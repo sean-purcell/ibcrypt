c903fee0f43f845f87fcd4901ea4b74984b2d2e1
Iburinoc <iburinoc@gmail.com>
2014-05-04 23:14:05 -0400

    switched scrypt implementation to use my salsa20 core

--- scrypt.c
+++ scrypt.c
@@ -26,50 +26,29 @@ static void salsa20_8(uint32_t B[16]) {
 	uint32_t x[16];
 	
 	memcpy(x, B, 64);
+#define ROT(x, n) (((x) << n) | ((x) >> (32 - n)))
+#define QROUND(a,b,c,d) \
+	((b) ^= ROT((a+d),  7));\
+	((c) ^= ROT((b+a),  9));\
+	((d) ^= ROT((c+b), 13));\
+	((a) ^= ROT((d+c), 18));
 	
-	for (int i = 0; i < 8; i += 2) {
-		/* columns */
-		x[ 4] ^= ROTL(x[ 0]+x[12], 7);
-		x[ 8] ^= ROTL(x[ 4]+x[ 0], 9);
-		x[12] ^= ROTL(x[ 8]+x[ 4],13);
-		x[ 0] ^= ROTL(x[12]+x[ 8],18);
-
-		x[ 9] ^= ROTL(x[ 5]+x[ 1], 7);
-		x[13] ^= ROTL(x[ 9]+x[ 5], 9);
-		x[ 1] ^= ROTL(x[13]+x[ 9],13);
-		x[ 5] ^= ROTL(x[ 1]+x[13],18);
-
-		x[14] ^= ROTL(x[10]+x[ 6], 7);
-		x[ 2] ^= ROTL(x[14]+x[10], 9);
-		x[ 6] ^= ROTL(x[ 2]+x[14],13);
-		x[10] ^= ROTL(x[ 6]+x[ 2],18);
-
-		x[ 3] ^= ROTL(x[15]+x[11], 7);
-		x[ 7] ^= ROTL(x[ 3]+x[15], 9);
-		x[11] ^= ROTL(x[ 7]+x[ 3],13);
-		x[15] ^= ROTL(x[11]+x[ 7],18);
-
-		/* rows */
-		x[ 1] ^= ROTL(x[ 0]+x[ 3], 7);
-		x[ 2] ^= ROTL(x[ 1]+x[ 0], 9);
-		x[ 3] ^= ROTL(x[ 2]+x[ 1],13);
-		x[ 0] ^= ROTL(x[ 3]+x[ 2],18);
-
-		x[ 6] ^= ROTL(x[ 5]+x[ 4], 7);
-		x[ 7] ^= ROTL(x[ 6]+x[ 5], 9);
-		x[ 4] ^= ROTL(x[ 7]+x[ 6],13);
-		x[ 5] ^= ROTL(x[ 4]+x[ 7],18);
-
-		x[11] ^= ROTL(x[10]+x[ 9], 7);
-		x[ 8] ^= ROTL(x[11]+x[10], 9);
-		x[ 9] ^= ROTL(x[ 8]+x[11],13);
-		x[10] ^= ROTL(x[ 9]+x[ 8],18);
-
-		x[12] ^= ROTL(x[15]+x[14], 7);
-		x[13] ^= ROTL(x[12]+x[15], 9);
-		x[14] ^= ROTL(x[13]+x[12],13);
-		x[15] ^= ROTL(x[14]+x[13],18);
+	/* iterate double round 20 times */
+	for(int i = 0; i < 8; i += 2) {
+		/* columnround */
+		QROUND(x[ 0], x[ 4], x[ 8], x[12]);
+		QROUND(x[ 5], x[ 9], x[13], x[ 1]);
+		QROUND(x[10], x[14], x[ 2], x[ 6]);
+		QROUND(x[15], x[ 3], x[ 7], x[11]);
+		
+		/* rowround */
+		QROUND(x[ 0], x[ 1], x[ 2], x[ 3]);
+		QROUND(x[ 5], x[ 6], x[ 7], x[ 4]);
+		QROUND(x[10], x[11], x[ 8], x[ 9]);
+		QROUND(x[15], x[12], x[13], x[ 14]);
 	}
+#undef ROT
+#undef QROUND
 	
 	for(int i = 0; i < 16; i++) {
 		B[i] += x[i];

7f03eb085b96911647545a81a82994130d266f0b
Iburinoc <iburinoc@gmail.com>
2014-05-09 17:55:18 -0400

    Switched to void pointer for password so that passing string constants is easier

--- scrypt.c
+++ scrypt.c
@@ -140,9 +140,10 @@ static void smix(uint8_t* B, uint64_t N, uint32_t r, uint32_t* V, uint32_t* X, u
 	}
 }
 
-int scrypt(uint8_t* pass, uint32_t plen, uint8_t* salt, uint32_t slen,
+int scrypt(void* _pass, uint32_t plen, uint8_t* salt, uint32_t slen,
 	uint64_t N, uint32_t r, uint32_t p, size_t dkLen, uint8_t* out) {
 	
+	uint8_t* pass = _pass;
 	uint8_t* B;
 	uint32_t* V;
 	uint32_t* X;

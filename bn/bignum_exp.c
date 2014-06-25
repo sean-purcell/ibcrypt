c571449578e749c8ef13551db229fecacff75420
Iburinoc <iburinoc@gmail.com>
2014-06-25 15:31:34 -0700

    added modular exponentiation.  doesn't quite work yet?

--- bn/bignum_exp.c
+++ bn/bignum_exp.c
@@ -35,3 +35,43 @@ int bno_exp(BIGNUM* r, const BIGNUM* base, const BIGNUM* exp) {
 
 	return bnu_trim(r);
 }
+
+int bno_exp_mod(BIGNUM* r, const BIGNUM* base, const BIGNUM* exp, const BIGNUM* n) {
+	if(r == 0 || base == 0 || exp == 0 || n == 0) {
+		return -1;
+	}
+
+	BIGNUM R[2] = { BN_ZERO, BN_ZERO };
+
+	if(bni_fstr(&R[0], "1") != 0) {
+		return 1;
+	}
+
+	if(bno_rmod(&R[1], base, n) != 0) {
+//	if(bni_cpy(&R[1], base)) {
+		return 1;
+	}
+
+	int64_t i;
+	for(i = exp->size - 1; i >= 0; i--) {
+		int j;
+		for(j = 63; j >= 0; j--) {
+			uint8_t bit = ((exp->d[i] & (1 << j)) >> j);
+			if(bno_mul_mod(&R[!bit], &R[0], &R[1], n) != 0) {
+				return 1;
+			}
+			if(bno_mul_mod(&R[bit], &R[bit], &R[bit], n) != 0) {
+				return 1;
+			}
+		}
+	}
+
+	if(bni_cpy(r, &R[0]) != 0) {
+		return 1;
+	}
+
+	bnu_free(&R[0]);
+	bnu_free(&R[1]);
+
+	return bnu_trim(r);
+}
